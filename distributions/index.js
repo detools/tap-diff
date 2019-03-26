aeb5736b5e833cfb752fced17d1646d56032774e
Anton Kuznetsov <isnifer@gmail.com>
2019-03-26 14:10:00 +0300

    [0.2.0] Update outdated packages

--- distributions/index.js
+++ distributions/index.js
@@ -1,55 +1,41 @@
-'use strict';
+"use strict";
 
-Object.defineProperty(exports, '__esModule', {
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.default = void 0;
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _diff = require("diff");
 
-var _diff = require('diff');
+var _chalk = _interopRequireDefault(require("chalk"));
 
-var _chalk = require('chalk');
+var _duplexer = _interopRequireDefault(require("duplexer"));
 
-var _chalk2 = _interopRequireDefault(_chalk);
+var _figures = _interopRequireDefault(require("figures"));
 
-var _duplexer = require('duplexer');
+var _through = _interopRequireDefault(require("through2"));
 
-var _duplexer2 = _interopRequireDefault(_duplexer);
+var _tapParser = _interopRequireDefault(require("tap-parser"));
 
-var _figures = require('figures');
+var _prettyMs = _interopRequireDefault(require("pretty-ms"));
 
-var _figures2 = _interopRequireDefault(_figures);
+var _jsondiffpatch = _interopRequireDefault(require("jsondiffpatch"));
 
-var _through2 = require('through2');
-
-var _through22 = _interopRequireDefault(_through2);
-
-var _tapParser = require('tap-parser');
-
-var _tapParser2 = _interopRequireDefault(_tapParser);
-
-var _prettyMs = require('pretty-ms');
-
-var _prettyMs2 = _interopRequireDefault(_prettyMs);
-
-var _jsondiffpatch = require('jsondiffpatch');
-
-var _jsondiffpatch2 = _interopRequireDefault(_jsondiffpatch);
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var INDENT = '  ';
-var FIG_TICK = _figures2['default'].tick;
-var FIG_CROSS = _figures2['default'].cross;
+var FIG_TICK = _figures.default.tick;
+var FIG_CROSS = _figures.default.cross;
 
 var createReporter = function createReporter() {
-  var output = (0, _through22['default'])();
-  var p = (0, _tapParser2['default'])();
-  var stream = (0, _duplexer2['default'])(p, output);
+  var output = (0, _through.default)();
+  var p = (0, _tapParser.default)();
+  var stream = (0, _duplexer.default)(p, output);
   var startedAt = Date.now();
 
   var println = function println() {
-    var input = arguments.length <= 0 || arguments[0] === undefined ? '' : arguments[0];
-    var indentLevel = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
-
+    var input = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+    var indentLevel = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
     var indent = '';
 
     for (var i = 0; i < indentLevel; ++i) {
@@ -57,20 +43,19 @@ var createReporter = function createReporter() {
     }
 
     input.split('\n').forEach(function (line) {
-      output.push('' + indent + line);
+      output.push("".concat(indent).concat(line));
       output.push('\n');
     });
   };
 
   var handleTest = function handleTest(name) {
     println();
-    println(_chalk2['default'].blue(name), 1);
+    println(_chalk.default.blue(name), 1);
   };
 
   var handleAssertSuccess = function handleAssertSuccess(assert) {
     var name = assert.name;
-
-    println(_chalk2['default'].green(FIG_TICK) + '  ' + _chalk2['default'].dim(name), 2);
+    println("".concat(_chalk.default.green(FIG_TICK), "  ").concat(_chalk.default.dim(name)), 2);
   };
 
   var toString = function toString(arg) {
@@ -78,12 +63,10 @@ var createReporter = function createReporter() {
   };
 
   var JSONize = function JSONize(str) {
-    return str
-    // wrap keys without quote with valid double quote
+    return str // wrap keys without quote with valid double quote
     .replace(/([\$\w]+)\s*:/g, function (_, $1) {
       return '"' + $1 + '":';
-    })
-    // replacing single quote wrapped ones to double quote
+    }) // replacing single quote wrapped ones to double quote
     .replace(/'([^']+)'/g, function (_, $1) {
       return '"' + $1 + '"';
     });
@@ -93,26 +76,22 @@ var createReporter = function createReporter() {
     var name = assert.name;
 
     var writeDiff = function writeDiff(_ref) {
-      var value = _ref.value;
-      var added = _ref.added;
-      var removed = _ref.removed;
+      var value = _ref.value,
+          added = _ref.added,
+          removed = _ref.removed;
+      var style = _chalk.default.white;
+      if (added) style = _chalk.default.green.inverse;
+      if (removed) style = _chalk.default.red.inverse; // only highlight values and not spaces before
 
-      var style = _chalk2['default'].white;
-
-      if (added) style = _chalk2['default'].green.inverse;
-      if (removed) style = _chalk2['default'].red.inverse;
-
-      // only highlight values and not spaces before
       return value.replace(/(^\s*)(.*)/g, function (m, one, two) {
         return one + style(two);
       });
     };
 
-    var _assert$diag = assert.diag;
-    var at = _assert$diag.at;
-    var actual = _assert$diag.actual;
-    var expected = _assert$diag.expected;
-
+    var _assert$diag = assert.diag,
+        at = _assert$diag.at,
+        actual = _assert$diag.actual,
+        expected = _assert$diag.expected;
     var expected_type = toString(expected);
 
     if (expected_type !== 'array') {
@@ -125,48 +104,47 @@ var createReporter = function createReporter() {
         }
       } catch (e) {
         try {
-          actual = JSON.stringify(eval('(' + actual + ')'), null, 2);
-          expected = JSON.stringify(eval('(' + expected + ')'), null, 2);
-        } catch (e) {
-          // do nothing because it wasn't a valid json object
+          actual = JSON.stringify(eval("(".concat(actual, ")")), null, 2);
+          expected = JSON.stringify(eval("(".concat(expected, ")")), null, 2);
+        } catch (e) {// do nothing because it wasn't a valid json object
         }
       }
 
       expected_type = toString(expected);
     }
 
-    println(_chalk2['default'].red(FIG_CROSS) + '  ' + _chalk2['default'].red(name) + ' at ' + _chalk2['default'].magenta(at), 2);
+    println("".concat(_chalk.default.red(FIG_CROSS), "  ").concat(_chalk.default.red(name), " at ").concat(_chalk.default.magenta(at)), 2);
 
     if (expected_type === 'object') {
-      var delta = _jsondiffpatch2['default'].diff(actual[failed_test_number], expected[failed_test_number]);
-      var _output = _jsondiffpatch2['default'].formatters.console.format(delta);
+      var delta = _jsondiffpatch.default.diff(actual[failed_test_number], expected[failed_test_number]);
+
+      var _output = _jsondiffpatch.default.formatters.console.format(delta);
+
       println(_output, 4);
     } else if (expected_type === 'array') {
       var compared = (0, _diff.diffJson)(actual, expected).map(writeDiff).join('');
-
       println(compared, 4);
     } else if (expected === 'undefined' && actual === 'undefined') {
       ;
     } else if (expected_type === 'string') {
-      var compared = (0, _diff.diffWords)(actual, expected).map(writeDiff).join('');
+      var _compared = (0, _diff.diffWords)(actual, expected).map(writeDiff).join('');
 
-      println(compared, 4);
+      println(_compared, 4);
     } else {
-      println(_chalk2['default'].red.inverse(actual) + _chalk2['default'].green.inverse(expected), 4);
+      println(_chalk.default.red.inverse(actual) + _chalk.default.green.inverse(expected), 4);
     }
   };
 
   var handleComplete = function handleComplete(result) {
     var finishedAt = Date.now();
-
     println();
-    println(_chalk2['default'].green('passed: ' + result.pass + '  ') + _chalk2['default'].red('failed: ' + (result.fail || 0) + '  ') + _chalk2['default'].white('of ' + result.count + ' tests  ') + _chalk2['default'].dim('(' + (0, _prettyMs2['default'])(finishedAt - startedAt) + ')'));
+    println(_chalk.default.green("passed: ".concat(result.pass, "  ")) + _chalk.default.red("failed: ".concat(result.fail || 0, "  ")) + _chalk.default.white("of ".concat(result.count, " tests  ")) + _chalk.default.dim("(".concat((0, _prettyMs.default)(finishedAt - startedAt), ")")));
     println();
 
     if (result.ok) {
-      println(_chalk2['default'].green('All of ' + result.count + ' tests passed!'));
+      println(_chalk.default.green("All of ".concat(result.count, " tests passed!")));
     } else {
-      println(_chalk2['default'].red((result.fail || 0) + ' of ' + result.count + ' tests failed.'));
+      println(_chalk.default.red("".concat(result.fail || 0, " of ").concat(result.count, " tests failed.")));
       stream.isFailed = true;
     }
 
@@ -175,33 +153,25 @@ var createReporter = function createReporter() {
 
   p.on('comment', function (comment) {
     var trimmed = comment.replace('# ', '').trim();
-
     if (/^tests\s+[0-9]+$/.test(trimmed)) return;
     if (/^pass\s+[0-9]+$/.test(trimmed)) return;
     if (/^fail\s+[0-9]+$/.test(trimmed)) return;
     if (/^ok$/.test(trimmed)) return;
-
     handleTest(trimmed);
   });
-
   p.on('assert', function (assert) {
     if (assert.ok) return handleAssertSuccess(assert);
-
     handleAssertFailure(assert);
   });
-
   p.on('complete', handleComplete);
-
   p.on('child', function (child) {
     ;
   });
-
   p.on('extra', function (extra) {
-    println(_chalk2['default'].yellow(('' + extra).replace(/\n$/, '')), 4);
+    println(_chalk.default.yellow("".concat(extra).replace(/\n$/, '')), 4);
   });
-
   return stream;
 };
 
-exports['default'] = createReporter;
-module.exports = exports['default'];
+var _default = createReporter;
+exports.default = _default;
